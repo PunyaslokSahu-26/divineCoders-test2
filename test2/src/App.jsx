62b7907f2ab5945eb648455397647a78b8f1440b
Om Prasad Dash <omdash315@gmail.com>
2025-04-05 02:13:09 +0530

    first commit

--- test2/src/App.jsx
+++ test2/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import EmployeePerformanceDashboard from './Components/Employee'
+import Navbar from './Components/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="flex">
+        <Navbar/>
+        <EmployeePerformanceDashboard/>
+      </div>
       
     </>
   )
